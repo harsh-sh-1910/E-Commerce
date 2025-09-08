f64cb352c840af7d861d189358ff4ba4c9333f6a
harsh-sh-1910 <harshsh1910@gmail.com>
2025-09-08 15:00:08 +0530

    update commit

--- server/config/allowedOrigin.js
+++ server/config/allowedOrigin.js
@@ -3,5 +3,6 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://192.168.1.14:5173",
   "http://localhost:5174",
+  "https://e-commerce-ad.onrender.com",
 ];
 module.exports = allowedOrigins;
